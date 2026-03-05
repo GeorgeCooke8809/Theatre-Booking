d43fcfc4f689a93da0f1a3482d73fb24c896fde0
George_Cooke <georgecooke8809@gmail.com>
2026-03-05 15:59:18 +0000

    Updated todos

--- backend.py
+++ backend.py
@@ -33,14 +33,14 @@ class Backend:
         """
         An internal function to get the next available primary key in one of the SQL tables
         """
-        #TODO: Implement - 
+        #TODO: Implement - _get_next_ID
         pass
 
     def check_password(self, email: str, password_attempt: str) -> bool:
         """
         Checks if the entered password is correct for the email
         """
-        #TODO: Implement - 
+        #TODO: Implement - check_password
         pass
 
     def check_email_in_database(self, email: str) -> bool:
@@ -48,21 +48,21 @@ class Backend:
         Checks if the provided email is already in use.
         Returns true if is in database (cannot be accepted)
         """
-        #TODO: Implement - 
+        #TODO: Implement - check_email_in_database
         pass
 
     def create_user(self, first_name: str, last_name: str, email: str, phone: str, password: str) -> None:
         """
         Creates a new user
         """
-        #TODO: Implement - 
+        #TODO: Implement - create_user
         pass
 
     def get_all_performances(self, date_from: datetime.date = datetime.date.today()) -> list[str]:
         """
         Gets and returns a list of all performances from and including the date provided sorted by ascending date
         """
-        #TODO: Implement - 
+        #TODO: Implement - get_all_performances
         pass
 
     def get_all_performance_showings(self, performanceID: int) -> list[tuple[str, str]]:
@@ -70,28 +70,28 @@ class Backend:
         Gets and returns a list of all showings for a performance.
         Returns in the format [[showingID: int, showing date: str]]
         """
-        #TODO: Implement - 
+        #TODO: Implement - get_all_performance_showings
         pass
 
     def get_unavailable_seats(self, showingID: int) -> list[str]:
         """
         Gets and returns a list of all the unavailable seats for the specified showing
         """
-        #TODO: Implement - 
+        #TODO: Implement - get_unavailable_seats
         pass
 
     def book_seat(self, seatID: str, showingID: int, seat_type: str) -> None:
         """
         Books the specified seat for the specified showing
         """
-        #TODO: Implement - 
+        #TODO: Implement - book_seat
         pass
 
     def get_booking_price(self, userID: int, performanceID: int, child_seats: int, adult_seats: int, elderly_seats: int) -> float:
         """
         Calculates the price for a potential booking based on specified values, userID is used to check if user is special guest
         """
-        #TODO: Implement - 
+        #TODO: Implement - get_booking_price
         pass
 
     def get_user_bookings(self, userID: int, date_from: datetime.date = datetime.date.today()) -> list[tuple[int, str]]:
@@ -99,56 +99,56 @@ class Backend:
         Gets and returns a list of all bookings for a user after the specified date (defaults to today).
         Returns in format [(bookingID: int, booking title: str)]
         """
-        #TODO: Implement - 
+        #TODO: Implement - get_user_bookings
         pass
 
     def generate_pdf(self, bookingID: int) -> None:
         """
         Generates and saves a PDF ticket for the specified booking
         """
-        #TODO: Implement - 
+        #TODO: Implement - generate_pdf
         pass
 
     def add_event(self, title: str, description: str, child_price: float = 5, adult_price: float = 10, elderly_price: float = 5) -> int:
         """
         Adds the event and returns the performanceID for the new performance so that showings can be added
         """
-        #TODO: Implement - 
+        #TODO: Implement - add_event
         pass
 
     def add_showing(self, performanceID: int, date: datetime.date = datetime.date.today()) -> None:
         """
         Adds a new showing to for the performance with the given performanceID on the specified date
         """
-        #TODO: Implement - 
+        #TODO: Implement - add_showing
         pass
 
     def admin_get_showings(self, performanceID: int) -> list[tuple[int, str, list[str]]]:
         """
         Gets and returns all showings for an event and returns in the following format: [(showingID: int, showing date: str, showing attendees (names): list)]
         """
-        #TODO: Implement - 
+        #TODO: Implement - admin_get_showings
         pass
 
     def get_all_users(self) -> list[tuple[int, str, str, str, str]]:
         """
         Gets and returns a list of all users (sorted alphabetically by surname) in the following format [(userID: int, first name: str, last name: str, phone: str, type: str)]
         """
-        #TODO: Implement - 
+        #TODO: Implement - get_all_users
         pass
 
     def change_user_type(self, userID: int, new_type: str) -> None:
         """
         Changes the user type of the specified user to the one provided
         """
-        #TODO: Implement - 
+        #TODO: Implement - change_user_type
         pass
 
     def delete_user(self, userID: int) -> None:
         """
         Deletes the provided user
         """
-        #TODO: Implement - 
+        #TODO: Implement - delete_user
         pass
 
 if __name__ == "__main__":
