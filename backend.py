61ac4ed89738dacfcd4f99676a246fba399d8ef1
George_Cooke <georgecooke8809@gmail.com>
2026-03-05 15:43:53 +0000

    Basic code outline

--- backend.py
+++ backend.py
@@ -1,1 +1,139 @@
 import pyodbc
+import logging
+import datetime
+
+class Backend:
+    def __init__(self, database:str = "COLLEGE") -> None:
+        self.database = database
+
+    def _connection(self):
+        """
+        An internal function used for the context manager to connect to the database.
+        """
+
+        if self.database == "PERSONAL":
+            logging.debug("Connecting to personal (local) database")
+
+            cs = (
+                    "Driver={ODBC Driver 18 for SQL Server};"
+                    "Server=(localdb)\\TheatreBooking;"
+                    "Database=TheatreBooking;"
+                    "Trusted_Connection=yes;"
+                )
+        else:
+            logging.debug("Connecting to college database")
+            logging.critical("College database has not yet been implemented!")
+            raise ImportError("College database has not yet been implemented.")
+         
+        logging.debug("Connected to database")
+
+        return pyodbc.connect(cs)
+
+    def _get_next_ID(self, table: str) -> int:
+        """
+        An internal function to get the next available primary key in one of the SQL tables
+        """
+        pass
+
+    def check_password(self, email: str, password_attempt: str) -> bool:
+        """
+        Checks if the entered password is correct for the email
+        """
+        pass
+
+    def check_email_in_database(self, email: str) -> bool:
+        """
+        Checks if the provided email is already in use.
+        Returns true if is in database (cannot be accepted)
+        """
+        pass
+
+    def create_user(self, first_name: str, last_name: str, email: str, phone: str, password: str) -> None:
+        """
+        Creates a new user
+        """
+        pass
+
+    def get_all_performances(self, date_from: datetime.date = datetime.date.today()) -> list[str]:
+        """
+        Gets and returns a list of all performances from and including the date provided sorted by ascending date
+        """
+        pass
+
+    def get_all_performance_showings(self, performanceID: int) -> list[tuple[str, str]]:
+        """
+        Gets and returns a list of all showings for a performance.
+        Returns in the format [[showingID: int, showing date: str]]
+        """
+        pass
+
+    def get_unavailable_seats(self, showingID: int) -> list[str]:
+        """
+        Gets and returns a list of all the unavailable seats for the specified showing
+        """
+        pass
+
+    def book_seat(self, seatID: str, showingID: int, seat_type: str) -> None:
+        """
+        Books the specified seat for the specified showing
+        """
+        pass
+
+    def get_booking_price(self, userID: int, performanceID: int, child_seats: int, adult_seats: int, elderly_seats: int) -> float:
+        """
+        Calculates the price for a potential booking based on specified values, userID is used to check if user is special guest
+        """
+        pass
+
+    def get_user_bookings(self, userID: int, date_from: datetime.date = datetime.date.today()) -> list[tuple[int, str]]:
+        """
+        Gets and returns a list of all bookings for a user after the specified date (defaults to today).
+        Returns in format [(bookingID: int, booking title: str)]
+        """
+        pass
+
+    def generate_pdf(self, bookingID: int) -> None:
+        """
+        Generates and saves a PDF ticket for the specified booking
+        """
+        pass
+
+    def add_event(self, title: str, description: str, child_price: float = 5, adult_price: float = 10, elderly_price: float = 5) -> int:
+        """
+        Adds the event and returns the performanceID for the new performance so that showings can be added
+        """
+        pass
+
+    def add_showing(self, performanceID: int, date: datetime.date = datetime.date.today()) -> None:
+        """
+        Adds a new showing to for the performance with the given performanceID on the specified date
+        """
+        pass
+
+    def admin_get_showings(self, performanceID: int) -> list[tuple[int, str, list[str]]]:
+        """
+        Gets and returns all showings for an event and returns in the following format: [(showingID: int, showing date: str, showing attendees (names): list)]
+        """
+        pass
+
+    def get_all_users(self) -> list[tuple[int, str, str, str, str]]:
+        """
+        Gets and returns a list of all users (sorted alphabetically by surname) in the following format [(userID: int, first name: str, last name: str, phone: str, type: str)]
+        """
+        pass
+
+    def change_user_type(self, userID: int, new_type: str) -> None:
+        """
+        Changes the user type of the specified user to the one provided
+        """
+        pass
+
+    def delete_user(self, userID: int) -> None:
+        """
+        Deletes the provided user
+        """
+        pass
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, filename="log.log", filemode="w",
+                        format="%(asctime)s - %(levelname)s - %(message)s")
---
+++ main.py
@@ -0,0 +1,5 @@
+import backend
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename="log.log", filemode="w",
+                        format="%(asctime)s - %(levelname)s - %(message)s")
