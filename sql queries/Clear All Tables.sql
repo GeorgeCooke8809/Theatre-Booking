c24b7d9933b01bf21b3d35af45044080d41a471d
George_Cooke <georgecooke8809@gmail.com>
2026-03-13 13:28:40 +0000

    Hopefully didn't break it?

--- sql queries/Clear All Tables.sql
+++
@@ -1,6 +0,0 @@
-TRUNCATE TABLE dbo.Users;
-TRUNCATE TABLE dbo.Bookings;
-TRUNCATE TABLE dbo.BookingSeats;
-TRUNCATE TABLE dbo.Performances;
-TRUNCATE TABLE dbo.PerformanceUnavailableSeats;
-TRUNCATE TABLE dbo.Showings;
