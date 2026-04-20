2ed9fd250d739b7ceecfc5b69c8591a6dce7c821
George_Cooke <georgecooke8809@gmail.com>
2026-04-20 14:47:27 +0100

    Added functionality to search in admin users

--- static/js/admin-users.js
+++ static/js/admin-users.js
@@ -13,7 +13,6 @@ async function updateUser(userID){
             });
 
     response_json = await response.json();
-    console.log(response_json);
 
     if (response_json.code == 200){
         console.log("Information correct, redirecting.");
@@ -43,33 +42,76 @@ async function search(term){
     console.log(response_json)
 
     if (response_json.code == 200){
-        console.log("Information correct, inserting elements.")
 
         container = document.querySelector("#users-section");
         container.innerHTML = '';
         
-        for (let i=0; i < (response_json.elements.length); i++){
-            let user = response_json.elements[i]
-            console.log(user)
-
-            let card = document.createElement("div")
-            card.classList.add("user-card", "inline-split")
-            console.log(card)
-            
-            let heading = document.createElement("h4")
-            heading.textContent = `${ user[1] } ${ user[2] } - ${ user[3] }`
-            card.appendChild(heading)
-
-            let form = document.createElement("form")
-            form.classList.add("inline", "user-type-form")
-            form.id = `update-${user[0]}` /* TODO: Contiunue generating form */
-
-            container.appendChild(card)
-        }
+        updateUsersList(response_json.elements)
     }
     else{
         console.log("Information incorrect, flashing error.")
         
         flashMessage(response_json.message)
+    }
+}
+
+function updateUsersList(users){
+    for (let i=0; i < (users.length); i++){
+        let user = users[i]
+
+        let card = document.createElement("div")
+        card.classList.add("user-card", "inline-split")
+        
+        let heading = document.createElement("h4")
+        heading.textContent = `${ user[1] } ${ user[2] } - ${ user[3] }`
+        card.appendChild(heading)
+
+        let form = document.createElement("form")
+        form.classList.add("inline", "user-type-form")
+        form.id = `update-${user[0]}`
+        form.setAttribute("onsubmit", `updateUser('${user[0]}')`)
+
+        let select = document.createElement("select")
+        select.className = "visitor-new-type"
+        select.name = "user-type"
+
+        let options = [["VISITOR", "Visitor"], ["SPECIAL", "Special"], ["ADMIN", "Admin"]]
+
+        for (j = 0; j < options.length; j++){
+            option = options[j]
+
+            option_elem = document.createElement("option")
+            option_elem.value = option[0]
+
+            if (user[4] == option[0]){
+                option_elem.selected = "selected"
+                option_elem.textContent = `${option[1]}*`
+            }
+            else{
+                option_elem.textContent = option[1]
+            }
+
+            select.appendChild(option_elem)
+        }
+
+        let delete_option = document.createElement("option")
+        delete_option.value = "DELETE"
+        delete_option.textContent = "DELETE"
+        delete_option.className = "delete-user"
+
+        select.appendChild(delete_option)
+
+        form.appendChild(select)
+
+        let form_submit = document.createElement("input")
+        form_submit.type = "submit"
+        form_submit.value = "Submit"
+        form_submit.className = "basic-button"
+
+        form.appendChild(form_submit)
+
+        card.appendChild(form)
+
+        container.appendChild(card)
     }
 }
